e05d82ee66d91d832b32fc9010450c19ffd0e3c8
Gary Burgmann <garyburgmann@icloud.com>
2021-06-03 11:23:48 +1000

    update provision script to copy config corectly to user dir

--- ubuntu.focal64/provision.sh
+++ ubuntu.focal64/provision.sh
@@ -23,9 +23,10 @@ IPADDR=192.168.33.11
 # https://github.com/flannel-io/flannel/blob/master/Documentation/kube-flannel.yml -> --pod-network-cidr=10.244.0.0/16
 kubeadm init --apiserver-cert-extra-sans=$IPADDR --node-name $NODENAME --pod-network-cidr=10.244.0.0/16
 sleep 30
-mkdir -p $HOME/.kube
-cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-chown $(id -u):$(id -g) $HOME/.kube/config
+mkdir -p /home/vagrant/.kube
+cp -i /etc/kubernetes/admin.conf /home/vagrant/.kube/config
+chown $(id -u):$(id -g) /home/vagrant/.kube/config
+echo "alias kc=kubectl" >> /home/vagrant/.bashrc
 kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
 kubectl taint nodes --all node-role.kubernetes.io/master-
 # export KUBECONFIG=/etc/kubernetes/admin.conf
