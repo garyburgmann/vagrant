5d5bc6565cfcb6320386d6bfa43f90f09b184ae1
Gary Burgmann <g.burgmann@griffith.edu.au>
2021-06-04 11:25:08 +1000

    fix kubeconfig setup for root and vagrant and comment out singularity install as it appears to mess with k8s install

--- ubuntu.focal64/bin/provision/install_k8s.sh
+++ ubuntu.focal64/bin/provision/install_k8s.sh
@@ -12,8 +12,14 @@ IPADDR=192.168.33.11
 # https://github.com/flannel-io/flannel/blob/master/Documentation/kube-flannel.yml -> --pod-network-cidr=10.244.0.0/16
 kubeadm init --apiserver-cert-extra-sans=$IPADDR --node-name $NODENAME --pod-network-cidr=10.244.0.0/16
 sleep 30
+# copy config for root
+mkdir -p $HOME/.kube
+cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+chown $(id -u):$(id -g) $HOME/.kube/config
+echo "alias kc=kubectl" >> $HOME/.bashrc
+# copy config for vagrant
 mkdir -p /home/vagrant/.kube
 cp -i /etc/kubernetes/admin.conf /home/vagrant/.kube/config
-chown $(id -u):$(id -g) /home/vagrant/.kube/config
+chown vagrant:vagrant /home/vagrant/.kube/config
 echo "alias kc=kubectl" >> /home/vagrant/.bashrc
 kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
