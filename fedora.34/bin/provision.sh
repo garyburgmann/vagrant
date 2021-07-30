5e2440854cdb6566a1d1ebfb6b3cc655d937b52f
Gary Burgmann <g.burgmann@griffith.edu.au>
2021-07-30 15:59:16 +1000

    update fedora.34 provision script

--- fedora.34/bin/provision.sh
+++ fedora.34/bin/provision.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
 sudo dnf update -y
 sudo dnf upgrade -y
-sudo dnf install -y podman podman-docker git wget
+sudo dnf install -y podman git wget
+# prepare container-related dev
 sudo curl https://raw.githubusercontent.com/containers/podman-compose/devel/podman_compose.py -o /usr/local/bin/podman-compose 
 sudo chmod +x /usr/local/bin/podman-compose
+curl -fsSL https://get.docker.com | bash
+sudo groupadd docker
+sudo usermod -aG docker vagrant
+sudo systemctl enable --now docker.service
+sudo systemctl enable --now containerd.service
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 wget https://repo.anaconda.com/archive/Anaconda3-2021.05-Linux-x86_64.sh -O /tmp/conda.sh
 sudo chmod +x /tmp/conda.sh
 /tmp/conda.sh -b -u -p $HOME/anaconda3
