f56e44d59d06144f07728b6a65d8531e53be59d5
Gary Burgmann <garyburgmann@gmail.com>
2021-08-01 22:30:59 +1000

    remove podman-docker install in fedora.34 due to conflict

--- fedora.34/bin/provision/install_podman.sh
+++ fedora.34/bin/provision/install_podman.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 sudo dnf update -y
-sudo dnf install -y podman podman-docker podman-compose
+sudo dnf install -y podman podman-compose
 
 systemctl --user enable --now podman.socket
