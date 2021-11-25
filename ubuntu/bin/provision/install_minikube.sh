3c4a3d2b5925279589b02fa4d79853cc40bc44f9
Gary Burgmann <garyburgmann@gmail.com>
2021-11-25 22:27:22 +1000

    typo fix - sudo

--- ubuntu/bin/provision/install_minikube.sh
+++ ubuntu/bin/provision/install_minikube.sh
@@ -1,4 +1,4 @@
-apt-get update && apt-get install -y curl
+sudo apt-get update && sudo apt-get install -y curl
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube_latest_amd64.deb
 sudo dpkg -i minikube_latest_amd64.deb
 rm minikube_latest_amd64.deb
